0636f4c333ac53bb536415bdeeb8b8193533811f
George Salu <george.salu10@gmail.com>
2019-12-03 16:07:04 -0300

    usando hooks - useState

--- jogo-busca-binaria/src/App.js
+++ jogo-busca-binaria/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [i, setI] = useState(1)
+
+  const increment = () => {
+    setI(i+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello DevPleno {i}</h1>
+      <button onClick={increment}>Incrementar</button>
     </div>
   );
 }
