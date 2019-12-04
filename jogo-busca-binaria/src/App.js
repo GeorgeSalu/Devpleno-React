cd3e0696a8bdefe3cd7195b263e84198087562c0
George Salu <george.salu10@gmail.com>
2019-12-04 12:15:50 -0300

    jogo busca binaria

--- jogo-busca-binaria/src/App.js
+++ jogo-busca-binaria/src/App.js
@@ -2,31 +2,64 @@ import React, { useState } from 'react';
 
 import './App.css';
 
-// elemento
-// JSX
-const elemento = <div><h2>Ola DevPleno</h2></div>
-const elemento2 = React.createElement('div', null, React.createElement('h2', null, 'Ola DevPleno'))
-
-const MostarI = (props) => {
-  return <p>{props.i}</p>
-}
-
-// componete: functional component
 function App() {
-  const [i, setI] = useState(1)
+  //ENTRADA, RODANDO, FIM
+  const [estado, setEstado] = useState('ENTRADA')
 
-  const increment = () => {
-    setI(i+1)
+  //palpite
+  const [palpite, setPalpite] = useState(150)
+  const [numPalpites, setNumPalpites] = useState(1)
+
+  const [min, setMin] = useState(0)
+  const [max, setMax] = useState(300)
+
+
+  const iniciarJogo = () => {
+    setEstado('RODANDO')
+    setMin(0)
+    setMax(300)
+    setNumPalpites(1)
+    setPalpite(150)
+  }
+
+  if(estado === 'ENTRADA') {
+    return <button onClick={iniciarJogo}>Começar a jogar</button>
+  }
+
+  const menor = () => {
+    setNumPalpites(numPalpites + 1)
+    setMax(palpite)
+    const proxPalpite = parseInt((palpite - min) / 2) + min
+    setPalpite(proxPalpite)
+  }
+
+  const maior = () => {
+    setNumPalpites(numPalpites + 1)
+    setMin(palpite)
+    const proxPalpite = parseInt((max - palpite) / 2) + palpite
+    setPalpite(proxPalpite)
+  }
+
+  const acertou = () => {
+    setEstado('FIM')
+  }
+
+  if(estado === 'FIM') {
+    return (
+      <div>
+        <p>Acertei o numero {palpite} com {numPalpites} chutes</p>
+        <button onClick={iniciarJogo}>Iniciar novamente</button>
+      </div>
+    )
   }
 
   return (
     <div className="App">
-      <h1>Hello DevPleno {i}</h1>
-      <button onClick={increment}>Incrementar</button>
-      <h2>Elementos</h2>
-      <MostarI i={i} />
-      {elemento}
-      {elemento2}
+      <p>O seu numero é {palpite}</p>
+      <p>Min : {min} / Max : {max}</p>
+      <button onClick={menor}>Menor</button>
+      <button onClick={acertou}>Acertou</button>
+      <button onClick={maior}>Maior</button>
     </div>
   );
 }
