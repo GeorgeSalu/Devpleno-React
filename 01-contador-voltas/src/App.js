32727224b8c6bb91f0eb9f64eec105cb7c3b1863
George Salu <george.salu10@gmail.com>
2019-12-04 12:52:39 -0300

    contador de voltas/reforçando o cronceito de props

--- 01-contador-voltas/src/App.js
+++ 01-contador-voltas/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+const MostrarVoltas = (props) => {
+  return (
+    <p>
+      {props.voltas} <br/>
+      Voltas
+    </p>
+  )
+}
+
+const MostarTempo = (props) => {
+  return (
+    <p>
+      {props.tempo}<br />
+      Tempo medio por volta
+    </p>
+  )
+}
+
+const Button = (props) => <button>{props.text}</button>
+
 function App() {
   return (
     <div className="App">
-      Reactjs
+      <MostrarVoltas voltas='15' />
+      <Button text='+' />
+      <Button text='-' />
+      <MostarTempo tempo='01:35' />
+      <Button text='Iniciar' />
+      <Button text='Reiniciar' />
     </div>
   );
 }
