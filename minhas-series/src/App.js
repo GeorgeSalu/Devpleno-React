9b7c562638a0f44f8c8d68a2aefe5f2958d79e51
George Salu <george.salu10@gmail.com>
2019-12-06 14:26:31 -0300

    primeira chamada com axios

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header'
+import axios from 'axios'
 
 import {
   BrowserRouter as Router,
@@ -15,12 +16,22 @@ const Generos = () => {
 }
 
 function App() {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, [])
+
   return (
     <Router>
       <div>
         <Header />
         <Route path='/' exact component={Home}/>
         <Route path='/generos'  component={Generos}/>
+        <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
   );
