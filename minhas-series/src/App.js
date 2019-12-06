722508d645ed231c89e5fdf5c4aefde248ff8d34
George Salu <george.salu10@gmail.com>
2019-12-06 12:13:24 -0300

    modularizando

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,34 +1,10 @@
-import React, { useState } from 'react';
-import { 
-  Navbar,
-  NavbarBrand, 
-  Collapse, 
-  Nav, 
-  NavItem, 
-  NavLink, 
-  NavbarToggler 
-} from 'reactstrap'
+import React from 'react';
+import Header from './Header'
 
 function App() {
-  const [open, setOpen] = useState(false)
-
-  const toggle = () => {
-    setOpen(!open)
-  }
-
   return (
     <div>
-      <Navbar color='ligth' light expand='md' >
-        <NavbarBrand >Minha Series</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={open} navbar>
-          <Nav className='ml-auto' navbar>
-            <NavItem>
-              <NavLink href='/' >Generos</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <Header />
     </div>
   );
 }
