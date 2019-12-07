5d9b4db2fbc64b2f1304931b02d74709e0326452
George Salu <george.salu10@gmail.com>
2019-12-07 20:23:29 -0300

    removendo hooks do App.js

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from './Header'
 import Generos from './Generos'
 import NovoGenero from './NovoGenero'
 import EditarGenero from './EditarGenero'
 import Series from './Series'
 import NovaSerie from './NovaSerie'
-import axios from 'axios'
 
 import {
   BrowserRouter as Router,
@@ -13,15 +12,6 @@ import {
 } from 'react-router-dom'
 
 function App() {
-
-  const [data, setData] = useState({})
-
-  useEffect(() => {
-    axios.get('/api').then(res => {
-      setData(res.data)
-    })
-  }, [])
-
   return (
     <Router>
       <div>
