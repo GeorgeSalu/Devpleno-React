09bc40a574d168af96885ebb77d66d38b17b2764
George Salu <george.salu10@gmail.com>
2019-12-07 17:42:12 -0300

    salvando generos

--- minhas-series/src/NovoGenero.js
+++ minhas-series/src/NovoGenero.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const NovoGenero = () => {
+  const [name, setName] = useState('')
+  const onChange = evt => {
+    setName(evt.target.value)
+  }
+  const save = () => {
+    axios.post('/api/genres', {
+      name,
+
+    })
+    .then(res => {
+      console.log(res)
+    })
+  }
   return (
     <div className='container'>
       <h1>Novo Genero</h1>
       <form>
         <div className='form-group'>
           <label for='name'>Nome</label>
-          <input type='text' className='form-control' id='name' placeholder='Nome do genero'></input>
+          <input type='text' value={name} onChange={onChange} className='form-control' id='name' placeholder='Nome do genero'></input>
         </div>
-        <button type='button' className='btn btn-primary'>Salvar Genero</button>
+        <button type='button' onClick={save} className='btn btn-primary'>Salvar Genero</button>
       </form>
     </div>
   )
