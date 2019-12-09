f3998e799726ec328e2e5efc70210acdecd847cb
George Salu <george.salu10@gmail.com>
2019-12-09 15:29:24 -0300

    aulao1 - exemplos

--- aulao1/src/App.js
+++ aulao1/src/App.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0)
+  const [counter2, setCounter2] = useState(10)
+
+  useEffect(() => {
+    console.log('useEffect')
+  }, [counter])
 
   return (
     <div className="App">
       <button onClick={() => setCounter(counter+1)}>incrementar {counter}</button>
+      <button onClick={() => setCounter2(counter2+1)}>incrementar {counter2}</button>
     </div>
   );
 }
