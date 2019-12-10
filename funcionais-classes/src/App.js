26648e57a6dd22a16bd6a5e7dbdee028c1ecb7bd
George Salu <george.salu10@gmail.com>
2019-12-10 14:05:27 -0300

    Componentes funcionais vs componentes Class ES6

--- funcionais-classes/src/App.js
+++ funcionais-classes/src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App2 extends React.Component {
+  render() {
+    return (
+      <h1>App2</h1>
+    )
+  }
+}
+
+const App1 = () => {
+  return <h1>Contador</h1>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <App1 />
+      <App2 />
     </div>
   );
 }
