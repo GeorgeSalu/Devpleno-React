933bc6b5fa80e2cbb6a8e9324d80cee7089e2c3d
George Salu <george.salu10@gmail.com>
2019-12-10 15:39:12 -0300

    projeto contador com classe es6

--- funcionais-classes/counter/src/App.js
+++ funcionais-classes/counter/src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './App.css'
 
 class App extends Component {
+  state = {
+    contador: 1
+  }
+  increment = () => {
+    this.setState({
+      contador: this.state.contador+1
+    })
+  }
+  decrement = () => {
+    this.setState({
+      contador: this.state.contador-1
+    })
+  }
+  clear = () => {
+    this.setState({
+      contador: 0
+    })
+  }
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
-          Contador : 0
+          Contador : {this.state.contador}
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.clear}>clear</button>
         </header>
       </div>
     )
