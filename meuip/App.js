020037b65157eb0a8994167bf354e90c3bc26c76
George Salu <george.salu10@gmail.com>
2020-07-07 09:57:17 -0300

    finalizando projeto meuip

--- meuip/App.js
+++ meuip/App.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Image} from 'react-native';
+import logo from './assets/logo.png'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.findMyIp = this.findMyIp.bind(this)
+
+    this.state = {
+      data: ''
+    }
+  }
+
+  async findMyIp() {
+    this.setState({
+      data: 'descobrindo ip...'
+    })
+    const ip = await fetch('http://gttpbin.org/ip')
+    const data = await ip.json()
+    this.setState({
+      data: data.origin
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.body}>
-          <Text style={styles.ip}>IP</Text>
-          <Button title="Descobrir meu ip"></Button>
+          <Image source={logo} />
+    <Text style={styles.ip}>{this.state.data}</Text>
+          <Button title="Descobrir meu ip" onPress={this.findMyIp}></Button>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.made}>Feito com amor</Text>
         </View>
       </View>
     );
@@ -30,5 +57,14 @@ const styles = StyleSheet.create({
     color: 'white',
     paddingTop: 20,
     paddingBottom: 20
+  },
+  footer: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },  
+  made: {
+    color: 'white',
+    textAlign: 'center',
+
   }
 });
