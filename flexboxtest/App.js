0db093fc84a0f266f0ab7f2ad0f9b8b7e6bb70bb
George Salu <george.salu10@gmail.com>
2020-07-07 16:22:56 -0300

    layouts e flexbox

--- flexboxtest/App.js
+++ flexboxtest/App.js
@@ -5,9 +5,9 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
       </View>
     );
   }
@@ -21,8 +21,21 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     justifyContent: 'space-between',
   },
-  box: {
-    padding: 30,
+  box1: {
+    //padding: 30,
+    flex: 1,
+    backgroundColor: 'steelblue',
+    margin: 4
+  },
+  box2: {
+    flex: 2,
+    //padding: 30,
+    backgroundColor: 'steelblue',
+    margin: 4
+  },
+  box3: {
+    flex: 3,
+    //padding: 30,
     backgroundColor: 'steelblue',
     margin: 4
   }
