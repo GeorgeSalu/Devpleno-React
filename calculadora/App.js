87c543a3d0c9ca7215339aa6f58d110129ace1a5
George Salu <george.salu10@gmail.com>
2020-07-08 11:33:29 -0300

    template calculadora

--- calculadora/App.js
+++ calculadora/App.js
@@ -5,8 +5,11 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text >Welcome to React Native!</Text>
-        <Text >To get started, edit App.js</Text>
+        <Text style={styles.display}>Display</Text>
+        <Text style={styles.result}>Result</Text>
+        <View style={styles.buttons} >
+
+        </View>
       </View>
     );
   }
@@ -15,8 +18,28 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  display: {
+    flex: 1,
+    backgroundColor: '#EFEFEF',
+    fontSize: 80,
+    textAlign: 'right',
+    paddingTop: 30,
+    paddingRight: 10
+  },
+  result: {
+    flex: 0.4,
+    backgroundColor: '#EFEFEF',
+    fontSize: 40,
+    textAlign: 'right',
+    paddingRight: 10,
+    paddingBottom: 10
+  },
+  buttons: {
+    flex: 5,
+    flexDirection: 'row'
   }
 });
