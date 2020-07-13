f892529b5d9f27137bb7dbb539e3a3414ea70adf
George Salu <george.salu10@gmail.com>
2020-07-13 16:36:57 -0300

    criando a tela EMOM

--- calistimer/App.js
+++ calistimer/App.js
@@ -6,6 +6,6 @@ import EMOMScreen from './src/screens/EMOMScreen';
 const AppNavigator = createStackNavigator({
   Home: HomeScreen,
   EMOM: EMOMScreen
-}, {initialRouteName: 'Home'})
+}, {initialRouteName: 'EMOM'})
 
 export default createAppContainer(AppNavigator)
