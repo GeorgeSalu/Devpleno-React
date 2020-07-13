55d1334ce8c8769cd17ca3bc18057fb87cc23de6
George Salu <george.salu10@gmail.com>
2020-07-13 16:49:02 -0300

    criando a tela EMOM (funcionalidade) e componente Select

--- calistimer/src/screens/EMOMScreen.js
+++ calistimer/src/screens/EMOMScreen.js
@@ -1,10 +1,44 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
+const Select = props => {
+  return (
+    <View style={{flex: 1}}>
+      <Text style={styleSelect.label}>Label</Text>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
+        <TouchableOpacity style={styleSelect.optSelected}> 
+          <Text style={styleSelect.optLabel}>Opt1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styleSelect.optSelected}>
+          <Text style={styleSelect.optLabel}>Opt1</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styleSelect = StyleSheet.create({
+  label: {
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 24
+  },
+  optSelected: {
+    backgroundColor: 'white'
+  },
+  optLabel: {
+    color: 'white',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 24
+  }
+})
 
 const EMOMScreen = props => {
   return (
     <View style={styles.container}>
       <Text>EMOM Screen</Text>
+      <Select />
     </View>
   )
 }
@@ -16,7 +50,8 @@ EMOMScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D6304A'
+    backgroundColor: '#D6304A',
+    paddingTop: 200
   }
 })
 
