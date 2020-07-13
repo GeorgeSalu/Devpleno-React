cee705724f7fe775eb98710c112355c3f85fe492
George Salu <george.salu10@gmail.com>
2020-07-13 11:17:08 -0300

    navegação com react-nagivate

--- calistimer/App.js
+++ calistimer/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import HomeScreen from './src/screens/HomeScreen';
+import EMOMScreen from './src/screens/EMOMScreen';
 
 const AppNavigator = createStackNavigator({
-  Home: HomeScreen
+  Home: HomeScreen,
+  EMOM: EMOMScreen
 }, {initialRouteName: 'Home'})
 
 export default createAppContainer(AppNavigator)
