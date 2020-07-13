2aeec246c455fab9a91fea12f09c2349cada9e04
George Salu <george.salu10@gmail.com>
2020-07-13 11:55:33 -0300

    mudando pra TouchableOpacity

--- calistimer/src/screens/HomeScreen.js
+++ calistimer/src/screens/HomeScreen.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { View, Text, TouchableHighlight } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 const HomeScreen = props => {
   return (
     <View style={{ flex: 1 ,backgroundColor: '#D6304A' }}>
       <Text style={{ fontFamily: 'Ubuntu-Bold', fontSize: 48, textAlign: 'center', color: 'white', marginTop: 111 }} >Calistimer</Text>
-      <TouchableHighlight  onPress={ () => props.navigation.navigate('EMOM') }>
+      <TouchableOpacity  onPress={ () => props.navigation.navigate('EMOM') }>
         <Text style={{ color: 'white', fontFamily:'Ubuntu-Regular', fontSize: 24, textAlign: 'center' }}>EMOM</Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={ () => props.navigation.navigate('EMOM') }>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM') }>
         <Text>AMRAP</Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={ () => props.navigation.navigate('EMOM') }>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM') }>
         <Text>Isometria</Text>
-      </TouchableHighlight>
+      </TouchableOpacity>
     </View>
   )
 }
