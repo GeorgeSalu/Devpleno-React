ac60aa3c028ca2f474610a586a742bb1ba715efb
George Salu <george.salu10@gmail.com>
2020-07-13 14:01:20 -0300

    corrigindo import

--- calistimer/src/screens/HomeScreen.js
+++ calistimer/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import Button from '../componentes/Button'
+import Button from '../components/Button'
 
 const HomeScreen = props => {
   return (
