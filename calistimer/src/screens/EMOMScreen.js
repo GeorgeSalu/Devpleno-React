a8484696287c8764fd2b5e6b96839944059d0e73
George Salu <george.salu10@gmail.com>
2020-07-17 17:23:14 -0300

    corrigindo indetacao

--- calistimer/src/screens/EMOMScreen.js
+++ calistimer/src/screens/EMOMScreen.js
@@ -6,45 +6,45 @@ import Title from '../components/Title'
 const EMOMScreen = props => {
   return (
     <KeyboardAvoidingView style={{flex:1}} behavior='padding'>
-    <View style={styles.container}>
-      <Title title="EMOM" subTitle="Every Minute On The Minutes" />
-      <Image style={{ alignSelf: 'center', marginBottom: 17 }} source={require('../../assets/settings-cog.png')} />
-      <Select 
-        label='Alertas:'
-        current={0}
-        options={
-        [ 
-          {
-            id: 0,
-            label: 'Desligado'
-          }, 
-          {
-            id: 15, 
-            label: '15s'
-          },
-          { 
-            id: 30,
-            label: '30s' 
-          }, 
-          {
-            id: 45,
-            label: '45s'
+      <View style={styles.container}>
+        <Title title="EMOM" subTitle="Every Minute On The Minutes" />
+        <Image style={{ alignSelf: 'center', marginBottom: 17 }} source={require('../../assets/settings-cog.png')} />
+        <Select 
+          label='Alertas:'
+          current={0}
+          options={
+          [ 
+            {
+              id: 0,
+              label: 'Desligado'
+            }, 
+            {
+              id: 15, 
+              label: '15s'
+            },
+            { 
+              id: 30,
+              label: '30s' 
+            }, 
+            {
+              id: 45,
+              label: '45s'
+            }
+          ]
           }
-        ]
-        }
-        onSelect={ opt => console.log('selecionado', opt) } />
+          onSelect={ opt => console.log('selecionado', opt) } />
 
-      <Select 
-        label='Contagem regressiva'
-        current={0}
-        options={[{ id: 1, label: 'sim'},{ id: 0, label:'não'}]}
-        onSelect={ opt => console.log('selecionado', opt) } />
-      <Text style={styles.label}>Quantod minutos:</Text>
-      <TextInput style={styles.input} keyboardType='numeric'>15</TextInput>
-      <Text style={styles.label}>minutos</Text>
-      <Image style={{ alignSelf: 'center' }} source={require('../../assets/btn-play.png')} />
-      <Text>Testar</Text>
-    </View>
+        <Select 
+          label='Contagem regressiva'
+          current={0}
+          options={[{ id: 1, label: 'sim'},{ id: 0, label:'não'}]}
+          onSelect={ opt => console.log('selecionado', opt) } />
+        <Text style={styles.label}>Quantod minutos:</Text>
+        <TextInput style={styles.input} keyboardType='numeric'>15</TextInput>
+        <Text style={styles.label}>minutos</Text>
+        <Image style={{ alignSelf: 'center' }} source={require('../../assets/btn-play.png')} />
+        <Text>Testar</Text>
+      </View>
     </KeyboardAvoidingView>
   )
 }
