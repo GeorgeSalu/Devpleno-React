a07f1779e18c060e23f2f034b910c802df6ed255
George Salu <george.salu10@gmail.com>
2020-07-17 17:02:29 -0300

    continuando tela EMOM

--- calistimer/src/screens/EMOMScreen.js
+++ calistimer/src/screens/EMOMScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Image, Text } from 'react-native'
 import Select from '../components/Select'
 import Title from '../components/Title'
 
@@ -7,6 +7,7 @@ const EMOMScreen = props => {
   return (
     <View style={styles.container}>
       <Title title="EMOM" subTitle="Every Minute On The Minutes" />
+      <Image source={require('../../assets/settings-cog.png')} />
       <Select 
         label='Alertas:'
         current={0}
@@ -37,6 +38,10 @@ const EMOMScreen = props => {
         current={0}
         options={[{ id: 1, label: 'sim'},{ id: 0, label:'não'}]}
         onSelect={ opt => console.log('selecionado', opt) } />
+      <Text style={styles.label}>Quantod minutos:</Text>
+      <Text style={styles.input}>15</Text>
+      <Text style={styles.label}>minutos</Text>
+      <Image source={require('../../assets/btn-play.png')} />
     </View>
   )
 }
@@ -50,7 +55,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#D6304A',
     paddingTop: 200
+  },
+  label: {
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 24
+  },
+  input: {
+    textAlign: 'center',
+    color: 'black',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 48
   }
+
 })
 
 export default EMOMScreen;
