449c8c575658f20deb38cd3d50bb7b2ec0246d33
George Salu <george.salu10@gmail.com>
2020-07-23 19:55:32 -0300

    criando compontente AMRAPScreen

--- calistimer/App.js
+++ calistimer/App.js
@@ -3,11 +3,13 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 import HomeScreen from './src/screens/HomeScreen';
 import EMOMScreen from './src/screens/EMOMScreen';
 import IsometriaScreen from './src/screens/IsometriaScreen';
+import AMRAPScreen from './src/screens/AMRAPScreen'
 
 const AppNavigator = createStackNavigator({
   Home: HomeScreen,
   EMOM: EMOMScreen,
-  Isometria: IsometriaScreen
-}, {initialRouteName: 'Home'})
+  Isometria: IsometriaScreen,
+  AMRAP: AMRAPScreen
+}, {initialRouteName: 'AMRAP'})
 
 export default createAppContainer(AppNavigator)
