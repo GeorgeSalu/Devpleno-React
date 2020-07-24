6a19cca1de3dc751118e1caf0e13289ffac0ec9d
George Salu <george.salu10@gmail.com>
2020-07-24 14:44:21 -0300

    tela sobre

--- calistimer/src/screens/AboutScreen.js
+++ calistimer/src/screens/AboutScreen.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native'
 
 const AboutScreen = props => {
+    const back = () => {
+        props.navigation.goBack()
+    }
+    const openURL = url => () =>{
+        Linking.openURL(url)
+    }
     return(
         <View style={styles.container}>
             <Text style={styles.logo}>CalisTimer</Text>
             <Text style={styles.description}>Este aplicativo foi construtído durante as aulas do curso de ReactJS/React-Native do DevPleno, o devReactJS nos módulos de react-native.</Text>
+            <TouchableOpacity onPress={openURL('https://devpleno.com')}>
+                <Image source={require('../../assets/devpleno.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={openURL('https://devpleno.com/devreactjs')}>
+                <Image source={require('../../assets/devreactjs.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={back}>
+                <Image source={require('../../assets/left-arrow.png')} />
+            </TouchableOpacity>
         </View>
     )
 }
