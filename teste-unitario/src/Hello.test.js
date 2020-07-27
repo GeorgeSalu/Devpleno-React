08f40426fcd3a3071809c93d3bbbb7801564b9d7
George Salu <george.salu10@gmail.com>
2020-07-27 09:11:56 -0300

    teste de renderização

--- teste-unitario/src/Hello.test.js
+++ teste-unitario/src/Hello.test.js
@@ -9,3 +9,28 @@ beforeEach(() => {
   document.body.appendChild(container)
 })
 
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+it('renders the name', () => {
+
+  act(() => {
+    ReactDOM.render(<Hello name='Tulio' />, container)
+  })
+
+  const p = container.querySelector('p')
+  expect(p.textContent).toBe('Ola Tulio!')
+})
+
+it('renders no name', () => {
+
+  act(() => {
+    ReactDOM.render(<Hello />, container)
+  })
+
+  const p = container.querySelector('p')
+  expect(p.textContent).toBe('Ola !')
+})
