846d19484b802d9fdb05157f841e8fc2e986f07b
George Salu <george.salu10@gmail.com>
2020-07-28 13:47:42 -0300

    componente input

--- devreact-ui/src/App.js
+++ devreact-ui/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Input } from './components';
 
 function App() {
   return (
     <div >
-      ReactJs
+      <h1>Input</h1>
+      <Input />
     </div>
   );
 }
