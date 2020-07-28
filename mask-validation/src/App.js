c0ba74ada41aab6fb93d59cd0bd436758e35626d
George Salu <george.salu10@gmail.com>
2020-07-28 09:10:43 -0300

    mascaras em input - parte 1

--- mask-validation/src/App.js
+++ mask-validation/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputMask from 'react-text-mask'
 
+const InputCEP = (props) => {
+  const mask = [/\d/, /\d/, '.',/\d/,/\d/,/\d/,'-',/\d/,/\d/,/\d/];
+  return (
+    <InputMask
+        guide
+        mask={mask} {...props} />
+  )
+}
+
+const InputCPF = (props) => {
+  const mask = [/\d/,/\d/, /\d/, '.',/\d/,/\d/,/\d/,'.',/\d/,/\d/,/\d/,'-',/\d/,/\d/];
+  return (
+    <InputMask
+        guide
+        mask={mask} {...props} />
+  )
+}
+
 function App() {
+  const [cep, setCEP] = useState('')
+  const [cpf, setCPF] = useState('')
   return (
     <div >
-      <InputMask
-        guide
-        mask={[/\d/, /\d/, /\d/]} />
+      <InputCEP onChange={evt => setCEP(evt.target.value)} />
+      CEP: {cep}
+      <InputCPF onChange={evt => setCPF(evt.target.value)} />
+      CPF: {cpf}
     </div>
   );
 }
