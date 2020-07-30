5dec85e323a5c5a9ee6011e9b3c43b18d1d81eee
George Salu <george.salu10@gmail.com>
2020-07-30 09:58:33 -0300

    redux logger

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import Counter from './Counter'
 import DisplayCounter from './DisplayCounter'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import counterReducer from './reducer'
+import logger from 'redux-logger'
 
 let store = createStore(
   counterReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger)
 )
 
 class App extends Component {
