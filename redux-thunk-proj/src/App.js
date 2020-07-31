20184cb616e4c19b6a09d3260c1cd23cf6f6d7f2
George Salu <george.salu10@gmail.com>
2020-07-31 09:21:13 -0300

    redux assincrono - preparando o projeto

--- redux-thunk-proj/src/App.js
+++ redux-thunk-proj/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+import {reducer} from './reducer'
+
+const store = createStore(
+  reducer
+)
+
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       react
     </div>
+    </Provider>
   );
 }
 
