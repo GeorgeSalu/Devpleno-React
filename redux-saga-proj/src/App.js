b7a8c753aea03fc29bdee61bc6b39199ae647c48
George Salu <george.salu10@gmail.com>
2020-08-03 10:51:24 -0300

    saga + axios

--- redux-saga-proj/src/App.js
+++ redux-saga-proj/src/App.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './reducers/index'
 import Info from './info'
+import axios from 'axios'
 
 import createSagaMiddleware from 'redux-saga'
 const sagaMiddleware = createSagaMiddleware()
@@ -14,6 +15,8 @@ const store = createStore(
 
 function *ola() {
   console.log('hello from saga')
+  const dados = yield axios.get('http://httpbin.org/ip')
+  console.log(dados)
 }
 sagaMiddleware.run(ola)
 
