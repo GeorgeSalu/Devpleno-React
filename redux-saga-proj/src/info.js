db2744787b275f5226a6ca1001ee5283e5addb76
George Salu <george.salu10@gmail.com>
2020-08-03 10:30:49 -0300

    componente Info

--- redux-saga-proj/src/info.js
+++ redux-saga-proj/src/info.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { loadDataRequest } from './actions'
+import { connect } from 'react-redux'
 
 class Info extends Component {
   render() {
@@ -6,4 +8,18 @@ class Info extends Component {
   }
 }
 
-export default Info
+const mapStateToProps = (state) => {
+  return {
+    isFetching: state.ip.isFetching,
+    data: state.ip.data,
+    error: state.ip.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadData: () => dispatch(loadDataRequest())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Info)
