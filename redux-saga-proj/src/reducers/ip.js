5e5fb2ee9091df01a23ac9e6b1f8fe28a663461b
George Salu <george.salu10@gmail.com>
2020-08-03 21:43:42 -0300

    saga effects - put e takeEvery

--- redux-saga-proj/src/reducers/ip.js
+++ redux-saga-proj/src/reducers/ip.js
@@ -12,6 +12,20 @@ const ip = (state = initial_state, action) => {
       error: false
     }
   }
+  if(action.type === 'LOAD_DATA_SUCCESS') {
+    return {
+      isFetching: false,
+      data: action.data,
+      error: false
+    }
+  }
+  if(action.type === 'LOAD_DATA_FAILURE') {
+    return {
+      isFetching: false,
+      data: [],
+      error: true
+    }
+  }
   return state
 }
 
