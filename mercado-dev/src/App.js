ae9e095e7690a28d0be3bcb93000b8f2b5ced3b3
George Salu <george.salu10@gmail.com>
2020-08-05 11:58:48 -0300

    mercado-dev - aula 1

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -2,10 +2,17 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends Component {
+  render() {
+    return <h1>MercadoDev</h1>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
