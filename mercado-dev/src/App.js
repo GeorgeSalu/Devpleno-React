ef57746c9a7f016ac471e6c8d847834859c5effe
George Salu <george.salu10@gmail.com>
2020-08-05 14:18:22 -0300

    projeto mercado-dev - componente linkCategoria

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HeaderHome from './HeaderHome'
 import AnuncioHome from './AnuncioHome'
 import Footer from './Footer'
+import LinkCategoria from './LinkCategoria'
 
 class App extends Component {
   render() {
@@ -10,7 +11,13 @@ class App extends Component {
       <div className="App">
         <HeaderHome />
         <div className="container">
-          <AnuncioHome />
+          <div className="row">
+            <AnuncioHome />
+          </div>
+          <h3>Categorias</h3>
+          <LinkCategoria />
+          <LinkCategoria />
+          <LinkCategoria />
         </div>
         <Footer />
       </div>
