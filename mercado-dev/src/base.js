21196646d881ffb16f641a7871403d912c046cd6
George Salu <george.salu10@gmail.com>
2020-08-05 15:32:35 -0300

    arquivo de config do firebase

---
+++ mercado-dev/src/base.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyCPJKneK3I3z_bt1gOXLdOZj9eBwtzqXMI",
+  authDomain: "mercadodev-c1e31.firebaseapp.com",
+  databaseURL: "https://mercadodev-c1e31.firebaseio.com",
+  projectId: "mercadodev-c1e31",
+  storageBucket: "mercadodev-c1e31.appspot.com",
+  messagingSenderId: "33962729639",
+  appId: "1:33962729639:web:fdfe380fe3b3697109bb82",
+  measurementId: "G-RM3GV6RJ9T"
+};
