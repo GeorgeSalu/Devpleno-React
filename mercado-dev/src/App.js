33d19a481a291b1f96bd725cd212417716ed9917
George Salu <george.salu10@gmail.com>
2020-08-05 16:46:51 -0300

    projeto mercado-dev - retornando categorias

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -4,9 +4,23 @@ import HeaderHome from './HeaderHome'
 import AnuncioHome from './AnuncioHome'
 import Footer from './Footer'
 import LinkCategoria from './LinkCategoria'
+import base from './base'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      categorias: []
+    }
+    base.bindToState('categorias', {
+      context: this,
+      state: 'categorias'
+    })
+  }
+
   render() {
+    let index = 0
     return (
       <div className="App">
         <HeaderHome />
@@ -16,9 +30,12 @@ class App extends Component {
           </div>
           <h3>Categorias</h3>
           <div className="row">
-            <LinkCategoria categoria={{ categoria: 'bicicleta', 'icon': 'fa-lightbulb-o' }} />
-            <LinkCategoria categoria={{ categoria: 'bicicleta', 'icon': 'fa-lightbulb-o' }} />
-            <LinkCategoria categoria={{ categoria: 'bicicleta', 'icon': 'fa-lightbulb-o' }} />
+            { this.state.categorias.map( (cat, indice) => {
+              return [
+                <LinkCategoria categoria={{ cat }} key={indice} />,
+                ++index%4 === 0 && <div key={'c'+indice} className="w-100"></div>
+              ]
+            } ) }
           </div>
         </div>
         <Footer />
