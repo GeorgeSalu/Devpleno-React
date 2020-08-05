316db61bad95163ba7128679e574f8dc9b681199
George Salu <george.salu10@gmail.com>
2020-08-05 15:00:18 -0300

    projeto mercado-dev - componente LinkCategoria

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -15,9 +15,8 @@ class App extends Component {
             <AnuncioHome />
           </div>
           <h3>Categorias</h3>
-          <LinkCategoria />
-          <LinkCategoria />
-          <LinkCategoria />
+          <LinkCategoria categoria={{ categoria: 'bicicleta', 'icon': 'fa-lightbulb-o' }} />
+
         </div>
         <Footer />
       </div>
