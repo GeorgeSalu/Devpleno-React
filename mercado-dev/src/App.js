4ff8aa57d60d66a5c0272c3290c2748805975cc8
George Salu <george.salu10@gmail.com>
2020-08-05 13:38:00 -0300

    componente HeaderHome e AnuncioHome

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -1,32 +1,15 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class Header extends Component {
-  render() {
-    return <h1>MercadoDev</h1>
-  }
-}
+import HeaderHome from './HeaderHome'
+import AnuncioHome from './AnuncioHome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HeaderHome />
+        <AnuncioHome />
       </div>
     );
   }
