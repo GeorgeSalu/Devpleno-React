940de52053c47f069f65b72cd0706d1b75349584
George Salu <george.salu10@gmail.com>
2020-08-06 09:55:59 -0300

    projeto mercado-dev - removendo imports nao utilizados

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import HeaderHome from './HeaderHome'
-import AnuncioHome from './AnuncioHome'
 import Footer from './Footer'
-import LinkCategoria from './LinkCategoria'
-import base from './base'
 import Home from './Home'
 
 class App extends Component {
