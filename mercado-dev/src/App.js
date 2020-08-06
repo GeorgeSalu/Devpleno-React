ced29765207592a7cdc0ebfeb80d54f84d669788
George Salu <george.salu10@gmail.com>
2020-08-06 15:32:38 -0300

    projeto mercado-dev - componente novoAnuncio

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Footer from './Footer'
 import Home from './Home'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NovoAnuncio from './NovoAnuncio'
 
 class App extends Component {
   render() {    
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Route path='/' exact component={Home} />
+          <Route path='/novo-anuncio' exact component={NovoAnuncio} />
           <Home />
           <Footer />
         </div>
---
+++ mercado-dev/src/NovoAnuncio.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class NovoAnuncio extends Component {
+  render() {
+    return (
+      <h1>Novo anuncio</h1>
+    )
+  }
+}
+
+export default NovoAnuncio
