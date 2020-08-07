752f6f1b5ad2a8dc0e9782bcf0583cf5465444bd
George Salu <george.salu10@gmail.com>
2020-08-07 17:30:02 -0300

    projeto mercado-dev - configurando storage

--- mercado-dev/src/base.js
+++ mercado-dev/src/base.js
@@ -3,7 +3,7 @@ const config = {
   authDomain: "mercadodev-c1e31.firebaseapp.com",
   databaseURL: "https://mercadodev-c1e31.firebaseio.com",
   projectId: "mercadodev-c1e31",
-  storageBucket: "mercadodev-c1e31.appspot.com",
+  storageBucket: "gs://mercadodev-c1e31.appspot.com",
   messagingSenderId: "33962729639",
   appId: "1:33962729639:web:fdfe380fe3b3697109bb82",
   measurementId: "G-RM3GV6RJ9T"
