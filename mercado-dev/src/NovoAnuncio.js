8750ad48c221f7fb4e5e7cf9111c8ff943bfc8a8
George Salu <george.salu10@gmail.com>
2020-08-07 09:58:01 -0300

    projeto mercado-dev - componente NovoAnuncio

--- mercado-dev/src/NovoAnuncio.js
+++ mercado-dev/src/NovoAnuncio.js
@@ -3,7 +3,15 @@ import React, {Component} from 'react'
 class NovoAnuncio extends Component {
   render() {
     return (
-      <h1>Novo anuncio</h1>
+      <div className="container" style={{ paddingTop: '120px' }}>
+        <h1>Novo anuncio</h1>
+        <form onSubmit={null}>
+          <div className="form-group">
+            <label htmlFor='nome'>Nome</label>
+            <input type='text' className='form-control' id='nome' placeholder='nome'></input>
+          </div>
+        </form>
+      </div>
     )
   }
 }
