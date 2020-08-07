6cb3153ca5c57e4f5967869d1aacb5cdfb37996f
George Salu <george.salu10@gmail.com>
2020-08-07 17:08:11 -0300

    projeto mercado-dev - lint

--- mercado-dev/src/HeaderInterno.js
+++ mercado-dev/src/HeaderInterno.js
@@ -10,7 +10,7 @@ const HeaderInterno = (props) => {
         <div className="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Link</a>
