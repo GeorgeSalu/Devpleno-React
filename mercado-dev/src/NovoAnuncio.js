7cef94f671b688993f1a45b62c2bc2754c900690
George Salu <george.salu10@gmail.com>
2020-08-07 10:34:40 -0300

    projeto mercado-dev - finalizando fomulario componente NovoAnuncio

--- mercado-dev/src/NovoAnuncio.js
+++ mercado-dev/src/NovoAnuncio.js
@@ -13,6 +13,23 @@ class NovoAnuncio extends Component {
               <label htmlFor='nome'>Nome</label>
               <input type='text' className='form-control' id='nome' placeholder='nome'></input>
             </div>
+            <div className="form-group">
+              <label htmlFor='descricao'>Desricao</label>
+              <input type='text' className='form-control' id='descricao' placeholder='descricao'></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor='preco'>preco</label>
+              <input type='text' className='form-control' id='preco' placeholder='preco'></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor='telefone'>Telefone</label>
+              <input type='text' className='form-control' id='telefone' placeholder='telefone'></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor='nome'>Vendedor</label>
+              <input type='text' className='form-control' id='vendedor' placeholder='vendedor'></input>
+            </div>
+            <button type='submit' className="btn btn-primary">Salvar</button>
           </form>
         </div>
       </div>
