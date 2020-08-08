a2ee56b876a767afdf44e83646e0f044d3ceaf1d
George Salu <george.salu10@gmail.com>
2020-08-08 16:01:34 -0300

    hands-on reactjs - projeto bora-ajudar - incluindo navegação

--- bora-ajudar/src/App.js
+++ bora-ajudar/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Header from './Header'
 import Home from './Home'
 import Footer from './Footer'
+import { 
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Route exact path='/' component={Home} />
+        <Home />
+        <Footer />
+      </Router>
     </div>
   );
 }
