c3535885adc38fc3c33fc980514d1cced1de6f08
George Salu <george.salu10@gmail.com>
2020-08-08 12:09:33 -0300

    hands-on reactjs - incluindo suporte a navegação

--- aplicacao1/src/App.js
+++ aplicacao1/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter'
@@ -19,24 +20,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            {this.state.counter}
-          </p>
-          <Counter counter={this.state.counter} />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+              {this.state.counter}
+            </p>
+            <Counter counter={this.state.counter} />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
