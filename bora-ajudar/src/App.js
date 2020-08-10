d41648bd2653c46557b2bc94ced880c048b9bd19
George Salu <george.salu10@gmail.com>
2020-08-10 10:56:50 -0300

    hands-on reactjs - projeto bora-ajudar - mudando App para componente de classe

--- bora-ajudar/src/App.js
+++ bora-ajudar/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { 
   BrowserRouter as Router,
   Route
@@ -11,19 +11,21 @@ import Sobre from './Sobre'
 import Contato from './Contato'
 import Campanhas from './Campanhas'
 
-function App() {
-  return (
-    <Router>
-      <div>
-          <Header />
-          <Route exact path='/' component={Home} />
-          <Route path='/sobre' exact component={Sobre} />
-          <Route path='/contato' exact component={Contato} />
-          <Route path='/campanhas' exact component={Campanhas} />
-          <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+            <Header />
+            <Route exact path='/' component={Home} />
+            <Route path='/sobre' exact component={Sobre} />
+            <Route path='/contato' exact component={Contato} />
+            <Route path='/campanhas' exact component={Campanhas} />
+            <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
