3ba676392f8643bcae3f42710129adf166d6378a
George Salu <george.salu10@gmail.com>
2020-08-10 10:35:51 -0300

    hands-on reactjs - projeto bora-ajudar - configurando firebase

---
+++ bora-ajudar/src/base.js
@@ -0,0 +1,17 @@
+import Rebase from 're-base'
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBSrqA_h_cF4QvZQodY8AomZKQhX1x9EEI",
+  authDomain: "bora-ajudar-b88c8.firebaseapp.com",
+  databaseURL: "https://bora-ajudar-b88c8.firebaseio.com",
+  projectId: "bora-ajudar-b88c8",
+  storageBucket: "bora-ajudar-b88c8.appspot.com",
+  messagingSenderId: "660328552656",
+  appId: "1:660328552656:web:6a498fe072f82df497a6d5"
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const base = Rebase.createClass(app.database())
+
+export default base
