160e0870fe635e5a115d84d921bc2fcedeede27e
George Salu <george.salu10@gmail.com>
2020-08-10 15:10:06 -0300

    hands-on reactjs - projeto bora-ajudar - criando componente Admin

--- bora-ajudar/src/Admin.js
+++ bora-ajudar/src/Admin.js
@@ -1,5 +1,36 @@
-import React from 'react'
+import React,{Component} from 'react'
+import base, {auth} from './base'
+import {Redirect} from 'react-router-dom'
 
-const Admin = props => <h1>Admin</h1>
+class Admin extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isAuthing: true,
+      isLoggedIn: false,
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({
+        isAuthing: false,
+        isLoggedIn: !!user,
+        user: user
+      })
+    })
+  }
+
+  render() {
+    if(this.state.isAuthing) {
+      return <p>Aguarde....</p>
+    }
+    if(!this.state.isLoggedIn) {
+      return <Redirect to='/login' />
+    }
+    return <h1>Admin</h1>
+  }
+}
 
 export default Admin
