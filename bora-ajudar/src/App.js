4a2a6151dfadb1eafa17d5b851f37d173db34166
George Salu <george.salu10@gmail.com>
2020-08-10 11:35:05 -0300

    hands-on reactjs - projeto bora-ajudar - testando conexao com firebase

--- bora-ajudar/src/App.js
+++ bora-ajudar/src/App.js
@@ -10,13 +10,30 @@ import Home from './Home'
 import Sobre from './Sobre'
 import Contato from './Contato'
 import Campanhas from './Campanhas'
+import base from './base';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contador: 1
+    }
+  }
+
+  componentDidMount() {
+    base.syncState('contador', {
+      context: this,
+      state: 'contador',
+      asArray: false
+    })
+  }
+
   render() {
     return (
       <Router>
         <div>
             <Header />
+            <di><h1>Contador: {this.state.contador}</h1></di>
             <Route exact path='/' component={Home} />
             <Route path='/sobre' exact component={Sobre} />
             <Route path='/contato' exact component={Contato} />
