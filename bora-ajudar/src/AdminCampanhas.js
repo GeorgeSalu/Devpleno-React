7cf2d413ff6a629eca190f940cd38cfcbe826cd4
George Salu <george.salu10@gmail.com>
2020-08-11 10:46:59 -0300

    hands-on reactjs - finalizando o salvar campanhas

--- bora-ajudar/src/AdminCampanhas.js
+++ bora-ajudar/src/AdminCampanhas.js
@@ -30,9 +30,13 @@ class AdminCampanhas extends Component {
   handleSave() {
     const nome = this.nome.value
     const descricao = this.descricao.value
+    const tipo = this.state.tipo
+    const comoDoar = (this.state.tipo === 'produtos' ? this.comoDoar.value : null)
+    const meta = (this.state.tipo === 'doacao' ? this.meta.value : null)
+    const doado = (this.state.tipo === 'doacao' ? this.doado.value : null)
 
     base.push('campanhas', {
-      data: {nome, descricao},
+      data: {nome, descricao, tipo, comoDoar, meta, doado},
       then: err => {
         if(!err) {
           this.nome.value = ''
@@ -58,7 +62,21 @@ class AdminCampanhas extends Component {
         <h1>Campanhas</h1>
         <h2>Nova Campanha</h2>
         Campanha: <input type="text" ref={ref => this.nome = ref} /><br />
+        SubTitulo: <input type="text" ref={ref => this.subTitulo = ref} /><br />
         Descricao: <textarea ref={ref => this.descricao = ref} ></textarea><br/>
+        Tipo: <br/> <input type='radio' name='tipo' onClick={() => this.setState({ tipo: 'doacao' })}  /> Doação <br />
+        <br/> <input type='radio' name='tipo' onClick={() => this.setState({ tipo: 'produtos' })} /> Produtos <br />
+        
+        { this.state.tipo === 'doacao' && <di>
+          <h4>Doacao</h4>
+          Meta : <input type='text' ref={ref => this.meta = ref} /><br/>
+          Doado : <input type='text' ref={ref => this.doado = ref} defaultValue={0} /><br/>
+        </di> }
+        { this.state.tipo === 'produtos' && <div>
+          <h4>Produtos</h4>
+          Como doar : <input type='text' ref={ref => this.comoDoar = ref} />
+        </div> }
+        
         <button onClick={this.handleSave}>Salvar nova campanha</button>
         <ul>
           { Object.keys(this.state.campanhas).map(key => this.renderCampanha(key, this.state.campanhas[key])) }
