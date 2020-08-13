0923b60cf6df2697a5e218831d276fa5e5d531ce
George Salu <george.salu10@gmail.com>
2020-08-13 10:36:50 -0300

    projeto jwt - decodificando token

--- jwt/src/App.js
+++ jwt/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 import './App.css';
 
 class App extends Component {
@@ -9,12 +10,18 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const login = await axios.post('http://localhost:3001/users/login', {
-      email: 'tuliofaria@devpleno.com',
-      passwd: 'abc123'
-    })
-    const token = login.data
-    console.log(token)
+    let token = localStorage.getItem('token')
+    if(!token) {
+      const login = await axios.post('http://localhost:3001/users/login', {
+        email: 'tuliofaria@devpleno.com',
+        passwd: 'abc123'
+      })
+      token = login.data.token
+      localStorage.setItem('token', token)
+    } else {
+      const decoded = jwtDecode(token)
+      console.log(decoded)
+    }
   }
 
   render() {
