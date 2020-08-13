686e764d2d761a3274961256d372fd972c33813b
George Salu <george.salu10@gmail.com>
2020-08-13 17:04:33 -0300

    projeto jwt - Routes com sub routes

--- jwt/src/screens/admin/index.js
+++ jwt/src/screens/admin/index.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
 
-const Admin = props => <h1>Admin</h1>
+const Home = props => <h1>Home</h1>
+const Users = props => <h1>Users</h1>
+
+const Admin = props => {
+  return (
+    <div>
+      <h1>Admin</h1>
+      <p>
+        <Link to='/admin'>Home</Link>
+        <Link to='/admin/users'>Users</Link>
+      </p>
+      <div>
+        <Route path={`${props.match.path}/`} exact component={Home} />
+        <Route path={`${props.match.path}/users`} exact component={Users} />
+      </div>
+    </div>
+  )
+}
 
 export default Admin
