58e78d4165bcd80bd1143ac3469e5bee32830a1f
George Salu <george.salu10@gmail.com>
2020-08-14 15:39:51 -0300

    projeto jwt - componente restrito

--- jwt/src/screens/restrito/index.js
+++ jwt/src/screens/restrito/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import {Redirect, Link, Route} from 'react-router-dom'
+
+const Home = props => <h1>Home</h1>
+const Users = props => <h1>Users</h1>
 
 const Restrito = props => {
   if(!props.auth.isAuth) {
     return <Redirect to='/login' />
   }
   return(
-    <h1>Restrito</h1>
+    <div>
+      <h1>Restrito</h1>
+      <p>
+        <Link to='/admin'>Home</Link>
+        <Link to='/admin/users'>Users</Link>
+      </p>
+      <div>
+        <Route path={`${props.match.path}/`} exact component={Home} />
+        <Route path={`${props.match.path}/users`} component={Users} />
+      </div>
+    </div>
   )
 }
 
