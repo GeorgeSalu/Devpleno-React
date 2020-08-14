dba64c0a876004dc5858a29d426494f2ac96c679
George Salu <george.salu10@gmail.com>
2020-08-13 21:46:31 -0300

    projeto jwt - criando componente login

--- jwt/src/screens/Login.js
+++ jwt/src/screens/Login.js
@@ -1,5 +1,36 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-const Login = props => <h1>Login</h1>
+class Login extends Component {
+
+  state = {
+    form: {
+      email: '',
+      passwd: ''
+    }
+  }
+
+  handleChange = fieldname => event =>{
+    const form = {
+      ...this.state.form
+    }
+    form[fieldname] = event.target.value
+    this.setState({ form })
+  }
+
+  login = () => {
+    console.log(this.state.form)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <input type='text' value={this.state.form.email} onChange={this.handleChange('email')} />
+        <input type='text' value={this.state.form.passwd} onChange={this.handleChange('passwd')} />
+        <button onClick={this.login}>Logar</button>
+      </div>
+    )
+  }
+}
 
 export default Login
