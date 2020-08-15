d797a4ee311ab248de08f16f2605caea81a4e728
George Salu <george.salu10@gmail.com>
2020-08-15 15:26:13 -0300

    projeto jwt - semantic ui conteudo

--- jwt/src/App.js
+++ jwt/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import store from './redux'
 import {Provider} from 'react-redux'
 import Header from './Header';
@@ -13,32 +12,22 @@ import Admin from './screens/admin'
 import Restrito from './screens/restrito'
 import Login from './screens/Login'
 
+import {Container} from 'semantic-ui-react'
+
 class App extends Component {
   render() {
     return (
       <Provider store={store} >
         <Router>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <Route exact path='/' component={Home} />
-              <Route path='/admin' component={Admin} />
-              <Route path='/restrito' component={Restrito} />
-              <Route path='/login' component={Login} />
-              <Header />
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
+          <Container>
+          
+            <Route exact path='/' component={Home} />
+            <Route path='/admin' component={Admin} />
+            <Route path='/restrito' component={Restrito} />
+            <Route path='/login' component={Login} />
+            <Header />
+            
+          </Container>
         </Router>
       </Provider>
     );
