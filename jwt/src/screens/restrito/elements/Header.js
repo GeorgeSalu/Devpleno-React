bad50f33412bbaf59545e9774f32e7d246f6106e
George Salu <george.salu10@gmail.com>
2020-08-17 14:49:35 -0300

    projeto jwt - dropdown menu

--- jwt/src/screens/restrito/elements/Header.js
+++ jwt/src/screens/restrito/elements/Header.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import ActionCreators from '../../../redux/actionCreators'
 
 import {Link} from 'react-router-dom'
-import {Menu} from 'semantic-ui-react'
+import {Menu, Dropdown} from 'semantic-ui-react'
 
 const Header = props => {
   return (
@@ -11,6 +11,15 @@ const Header = props => {
       <Menu.Item>Corridas Online <b>Admin</b></Menu.Item>
       <Menu.Item as={Link} to='/restrito'>Home</Menu.Item>
       <Menu.Item as={Link} to='/restrito/runs'>Corridas</Menu.Item>
+      <Menu.Menu position='right'>
+        <Dropdown item text={props.auth.user.name}>
+          <Dropdown.Menu>
+            <Dropdown.Item>Minha contsta</Dropdown.Item>
+            <Dropdown.Item>Alterar senha</Dropdown.Item>
+            <Dropdown.Item>sair</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Menu>
     </Menu>
   )
 }
