e5196e4f0423434b355aa0b64c56b07344ea448c
George Salu <george.salu10@gmail.com>
2020-08-18 11:07:14 -0300

    projeto jwt - componentens de data, duração e distancia

--- jwt/src/screens/elements/DataStr.js
+++ jwt/src/screens/elements/DataStr.js
@@ -4,7 +4,7 @@ import moment from 'moment-timezone'
 const DateStr = ({ date, timezone }) => {
   const d = moment.tz(date, 'GMT')
   const d2 = d.clone().tz(timezone)
-  return <span>{d2.format('MM/Do/YYYY')}</span>
+  return <span>{d2.format('DD/MM/YYYY H:mm:ss')}</span>
 }
 
 export default DateStr
