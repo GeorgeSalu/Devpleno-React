cc0dc1606576c032871b5a32450145ac45d16cc2
George Salu <george.salu10@gmail.com>
2020-08-24 13:58:26 -0300

    hands-on tripPlanner - componente HomeScreen

--- TripPlanner/src/screens/HomeScreen.js
+++ TripPlanner/src/screens/HomeScreen.js
@@ -1,12 +1,28 @@
 import React,{Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, ImageBackground, Image} from 'react-native'
 
 class HomeScreen extends Component {
   render() {
     return(
-      <View style={{paddingTop: 100}}>
-        <Text>Ola Hands-on rn!</Text>
-      </View>
+      <ImageBackground 
+        source={require('../assets/background.png')}
+        imageStyle={{ resizeMode: 'stretch' }}
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <View>
+          <Image source={require('../assets/logo-tripplanner.png')} />
+        </View>
+        <View>
+          <Image source={require('../assets/logo-devpleno.png')} />
+        </View>
+        <View>
+          <Text>comecar!</Text>
+        </View>
+      </ImageBackground>
     )
   }
 }
