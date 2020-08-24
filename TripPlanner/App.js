73cb8bd1ed214437cb580dbe093f074352ae22aa
George Salu <george.salu10@gmail.com>
2020-08-24 11:31:14 -0300

    hands-on tripPlanner - criando primeiro componente

--- TripPlanner/App.js
+++ TripPlanner/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-      </View>
+      <HomeScreen />
     );
   }
 }
---
+++ TripPlanner/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+import {View, Text} from 'react-native'
+
+class HomeScreen extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Ola Hands-on rn!</Text>
+      </View>
+    )
+  }
+}
+
+export default HomeScreen
