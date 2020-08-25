9960bfea61ce3188729d47e98d03e65378580141
George Salu <george.salu10@gmail.com>
2020-08-25 14:56:42 -0300

    hands-on tripPlanner - removendo import de Component nao necessario

--- TripPlanner/App.js
+++ TripPlanner/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import HomeScreen from './src/screens/HomeScreen'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 
