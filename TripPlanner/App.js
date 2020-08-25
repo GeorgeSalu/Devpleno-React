f8c839cb21f4d2f13f16d0aaea6712c2c0596b9b
George Salu <george.salu10@gmail.com>
2020-08-25 17:07:18 -0300

    hands-on tripPlanner - criando o tela trips

--- TripPlanner/App.js
+++ TripPlanner/App.js
@@ -1,8 +1,10 @@
-import HomeScreen from './src/screens/HomeScreen'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import HomeScreen from './src/screens/HomeScreen'
+import TripsScreen from './src/screens/TripsScreen'
 
 const AppNavigator = createStackNavigator({
-  Home: HomeScreen
+  Home: HomeScreen,
+  Trips: TripsScreen
 }, { initialRouteName: 'Home' })
 
 export default createAppContainer(AppNavigator)
---
+++ TripPlanner/src/screens/TripsScreen/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+class TripsScreen extends Component {
+  render() {
+    return(
+      <Text>Ola</Text>
+    )
+  }
+}
+
+export default TripsScreen
