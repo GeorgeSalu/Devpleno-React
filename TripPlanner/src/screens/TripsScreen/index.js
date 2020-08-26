dc5942e5485c2205f1cf59a822ec7fb252355eb4
George Salu <george.salu10@gmail.com>
2020-08-26 09:48:44 -0300

    hands-on tripPlanner - estilizando componente TripsScreen

--- TripPlanner/src/screens/TripsScreen/index.js
+++ TripPlanner/src/screens/TripsScreen/index.js
@@ -1,15 +1,53 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Dimensions} from 'react-native'
 
 class TripsScreen extends React.Component {
+
+  static navigationOptions = {
+    header: null
+  }
+
   render() {
+    const dim = Dimensions.get('window')
     return(
-      <View>
-        <View>
+      <View style={{
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'stretch'
+      }}>
+        <View style={{
+          backgroundColor: 'red',
+          flex: 1
+        }}>
           <Text>Mapa</Text>
         </View>
-        <View>
-          <Text>Lista de viagens</Text>
+        <View style={{
+          backgroundColor: 'pink'
+        }}>
+          <View style={{
+            backgroundColor: 'white',
+            paddingTop: 16,
+            paddingLeft: 16
+          }}>
+            <View style={{
+              backgroundColor: 'green',
+              width: dim.width-32,
+              height: 144
+            }}><Text>Image</Text></View>
+            <Text>Eurotrip 2019</Text>
+            <Text style={{
+              position: 'absolute',
+              top: 144-16,
+              right: 32,
+              textAlign: 'right',
+              backgroundColor: '#24C6DC',
+              paddingTop: 4,
+              paddingBottom: 4,
+              paddingRight: 4,
+              paddingLeft: 4,
+              color: 'white'
+            }}>R$ 200</Text>
+          </View>
         </View>
       </View>
     )
