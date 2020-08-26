a93d2869a910175dbfa22faa5f3a6bdcdbe381c4
George Salu <george.salu10@gmail.com>
2020-08-26 14:05:35 -0300

    hands-on tripPlanner - customizando componente TripScreen

--- TripPlanner/src/screens/TripScreen/index.js
+++ TripPlanner/src/screens/TripScreen/index.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react'
-import {View, Text, FlatList} from 'react-native'
+import {View, Text, FlatList, TouchableOpacity,Image , TouchableWithoutFeedback} from 'react-native'
 
 class TripScreen extends Component {
+
+  static navigationOptions = {
+    header: null
+  }
+
   renderItem = item => {
     return (
       <View>
@@ -22,11 +27,37 @@ class TripScreen extends Component {
       ]
     }
     return (
-      <View>
-        <View>
-          <Text>Header</Text>
+      <View style={{
+        flex: 1
+      }}>
+        <TouchableWithoutFeedback>
+          <Image source={require('../../assets/icon-chevron-left.png')} />
+        </TouchableWithoutFeedback>
+        <View style={{
+          height: 192,
+          backgroundColor: 'grey'
+        }}>
+          <Text style={{
+            position: 'absolute',
+            left: 16,
+            bottom: 16
+          }}>{trip.name}</Text>
+          <Text style={{
+              position: 'absolute',
+              bottom: 16,
+              right: 32,
+              textAlign: 'right',
+              backgroundColor: '#24C6DC',
+              paddingTop: 4,
+              paddingBottom: 4,
+              paddingRight: 4,
+              paddingLeft: 4,
+              color: 'white'
+          }}>R$ 5000</Text>
         </View>
-        <FlatList
+        <FlatList style={{
+          flex: 1
+        }}
           data={trip.places}
           renderItem={this.renderItem}
         />
