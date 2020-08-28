f6d3345de3c3cec68c4b29cf5266a348998164a6
George Salu <george.salu10@gmail.com>
2020-08-28 16:04:09 -0300

    hands-on tripPlanner - removendo imports nao utilizados em  AddTrip

--- TripPlanner/src/screens/AddTripScreen/index.js
+++ TripPlanner/src/screens/AddTripScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text, FlatList, TouchableOpacity,Image, TextInput } from 'react-native'
+import {View, Text, TouchableOpacity, TextInput } from 'react-native'
 import styles from './styles'
 
 export default class AddTripScreen extends React.Component {
