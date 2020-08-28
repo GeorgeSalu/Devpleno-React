340878a6e9da46f563ae2e74fb8a0b40dbaed77c
George Salu <george.salu10@gmail.com>
2020-08-28 15:43:34 -0300

    hands-on tripPlanner - criando o componente AddPointScreen

---
+++ TripPlanner/src/screens/AddPointScreen/index.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import {View, Text, FlatList, TouchableOpacity,Image, TextInput } from 'react-native'
+import MapView, {Marker} from 'react-native-maps'
+import styles from './styles'
+
+class AddPointScreen extends React.Component {
+
+  static navigationOptions = {
+    header: null
+  }
+
+  state = {
+    position: {
+      latitude: 37.78825,
+      longitude: -122.4324
+    },
+    pointName: '',
+    description: '',
+    price: 0
+  }
+
+  renderItem = item => {
+    return (
+      <View style={styles.item}>
+        <View style={styles.wrapperInfo}>
+          <Text style={styles.itemName}>{item.item.name}</Text>
+          <Text>{item.item.description}</Text>
+        </View>
+        <View style={styles.wrapperItemPice}>
+          <Text style={styles.itemPrice}>{item.item.price}</Text>
+        </View>
+      </View>
+    )
+  }
+
+  render() {
+    const trip = {
+      name: 'EuroTrip 2019',
+      price: 'R$ 5000',
+      places: [
+        {id: '1', name: 'Amsterdam', description: 'chegada',  price: 100, lat: 0, long: 0},
+        {id: '2', name: 'bruxelas', description: 'hospedagem', price: 1000, lat: 0, long: 0},
+      ]
+    }
+    return (
+      <View style={styles.wrapper}>
+        
+        <View style={styles.header}>
+            <MapView style={{flex: 1}} initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            }}>
+              <Marker 
+                coordinate={{
+                  latitude: 37.78825,
+                  longitude: -122.4324
+                }} 
+                draggable
+                onDragEnd={
+                  (evt) => this.setState({ position: evt.nativeEvent.coordinate })
+                }
+              />
+            </MapView>
+          <View style={styles.backButton}>
+            <TouchableOpacity onPress={() => this.props.navigation.goBack()} >
+              <Image source={require('../../assets/icon-chevron-left.png')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+          <TextInput style={styles.input} placeholder='Nome do ponto' onChangeText={txt => this.setState({ pointName: txt })}></TextInput>
+          <TextInput style={styles.input} placeholder='Descricao' onChangeText={txt => this.setState({ description: txt })}></TextInput>
+          <TextInput style={styles.input} placeholder='Preco' onChangeText={txt => this.setState({ price: txt })}></TextInput>
+          <TouchableOpacity style={styles.btn}>
+            <Text>Salvar ponto</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+export default AddPointScreen
