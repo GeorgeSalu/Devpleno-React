b48dfb9ed8e09486c4cb92591fe4e66235164071
George Salu <george.salu10@gmail.com>
2020-09-01 10:06:46 -0300

    projeto TripPlanner - corrigindo indentação

--- TripPlanner/src/screens/AddPointScreen/index.js
+++ TripPlanner/src/screens/AddPointScreen/index.js
@@ -64,11 +64,11 @@ class AddPointScreen extends React.Component {
       <View style={styles.wrapper}>
         <View style={styles.header}>
             <MapView style={{flex: 1}} initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421
-            }}>
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+              }}>
               <Marker 
                 coordinate={{
                   latitude: 37.78825,
